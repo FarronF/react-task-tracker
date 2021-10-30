4e2c9d89e3e9a22ea6c006bffdeefc172fcf76e1
Faz-PC\Faz <farronflaherty@gmail.com>
2021-10-30 23:41:33 +0100

    Delete task functionality

--- src/App.js
+++ src/App.js
@@ -6,31 +6,36 @@ import { useState } from "react"
 function App() {
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: 'Mandarin class',
-        date: new Date(2021, 10, 31, 12, 30, 0, 0),
-        reminder: true,
+      id: 1,
+      text: 'Mandarin class',
+      date: new Date(2021, 10, 31, 12, 30, 0, 0),
+      reminder: true,
     },
     {
-        id: 2,
-        text: 'D&D session',
-        date: new Date(2021, 10, 31, 17, 30, 0, 0),
-        reminder: true,
+      id: 2,
+      text: 'D&D session',
+      date: new Date(2021, 10, 31, 17, 30, 0, 0),
+      reminder: true,
     },
     {
-        id: 3,
-        text: 'Food shopping',
-        date: new Date(2021, 11, 1, 13, 30, 0, 0),
-        reminder: false,
+      id: 3,
+      text: 'Food shopping',
+      date: new Date(2021, 11, 1, 13, 30, 0, 0),
+      reminder: false,
     }
 
-])
-   return (
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  return (
     <div className="container">
       <Header title='Faz' />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
-    
+
   );
 }
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,11 @@
 import { FaTimes } from 'react-icons/fa';
 import dateFormat from 'dateformat';
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
     
     return (
         <div className='task'>
-            <h3>{task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}}/></h3>
+            <h3>{task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)}/></h3>
             <p>{dateFormat(task.date, 'dddd, mmmm dS, yyyy')}</p>
             <p>{dateFormat(task.date, 'HH:MM')}</p>
         </div>
