b6854525ff5097076bce7ce303e310694efb1884
Faz-PC\Faz <farronflaherty@gmail.com>
2021-10-30 21:35:09 +0100

    Initial setup. Reusable button component

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+
+const Button = ({color, text}) => {
+    return (
+        <button style={{backgroundColor: color}}className='btn'>{text}</button>
+    )
+}
+
+Button.defaultProps = {
+    color: 'black'
+}
+
+Button.propTypes = {
+    color: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+}
+
+export default Button
