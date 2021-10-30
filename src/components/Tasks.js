1ee17e0ec861e077dec5c0f8ca4738ff4c87bd66
Faz-PC\Faz <farronflaherty@gmail.com>
2021-10-30 21:57:50 +0100

    Moving tasks to global state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,6 @@
-import { useState } from "react"
 
 
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Mandarin class',
-            date: new Date(2021, 10, 31, 12, 30, 0, 0),
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'D&D session',
-            date: new Date(2021, 10, 31, 17, 30, 0, 0),
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Food shopping',
-            date: new Date(2021, 11, 1, 13, 30, 0, 0),
-            reminder: false,
-        }
-    
-    ])
+const Tasks = ({tasks}) => {
     return (
         <>
             {tasks.map(task => (<h3 key={task.id}>{task.text}</h3>))}
