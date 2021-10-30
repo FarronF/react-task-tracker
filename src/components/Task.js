d551d1112a9857182b271c62e7753df03c137043
Faz-PC\Faz <farronflaherty@gmail.com>
2021-10-31 00:04:11 +0100

    Reminder functionality

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,11 @@
 import { FaTimes } from 'react-icons/fa';
+import { FaBell } from 'react-icons/fa';
 import dateFormat from 'dateformat';
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
     
     return (
-        <div className='task'>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
             <h3>{task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)}/></h3>
             <p>{dateFormat(task.date, 'dddd, mmmm dS, yyyy')}</p>
             <p>{dateFormat(task.date, 'HH:MM')}</p>
