964b9420f3bce7a1c68ecd9b4a1998a2982e9869
Faz-PC\Faz <farronflaherty@gmail.com>
2021-10-30 21:53:13 +0100

    Moving tasks to state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,28 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Mandarin class',
-        date: new Date(2021, 10, 31, 12, 30, 0, 0),
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'D&D session',
-        date: new Date(2021, 10, 31, 17, 30, 0, 0),
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food shopping',
-        date: new Date(2021, 11, 1, 13, 30, 0, 0),
-        reminder: false,
-    }
+import { useState } from "react"
 
-]
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Mandarin class',
+            date: new Date(2021, 10, 31, 12, 30, 0, 0),
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'D&D session',
+            date: new Date(2021, 10, 31, 17, 30, 0, 0),
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Food shopping',
+            date: new Date(2021, 11, 1, 13, 30, 0, 0),
+            reminder: false,
+        }
+    
+    ])
     return (
         <>
             {tasks.map(task => (<h3 key={task.id}>{task.text}</h3>))}
