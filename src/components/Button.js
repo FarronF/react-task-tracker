535e78dd5b8608df728f6192ab2f475ae2224b2d
Faz-PC\Faz <farronflaherty@gmail.com>
2021-10-30 21:40:32 +0100

    Adding onClick log

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
+
+const Button = ({color, text, onClick}) => {
+    
     return (
-        <button style={{backgroundColor: color}}className='btn'>{text}</button>
+        <button onClick={onClick} style={{backgroundColor: color}}className='btn'>{text}</button>
     )
 }
 
@@ -12,7 +14,8 @@ Button.defaultProps = {
 
 Button.propTypes = {
     color: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    onClick: PropTypes.func
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({title}) => {
+    const  onClick = (e) => {
+        console.log(e)
+    }
+
     return (
         <header className='header'>
             <h1>{title} Task Tracker</h1>
-            <Button color='green' text='Add'/>
+            <Button color='green' text='Add' onClick={onClick}/>
             <Button color='red' text='Remove'/>
         </header>
     )
