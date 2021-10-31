d324022e2cb5f3b180db57047e0afc7cc6213e22
Faz-PC\Faz <farronflaherty@gmail.com>
2021-10-31 14:18:23 +0000

    USe json server as backend

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Mandarin class',
-      date: new Date(2021, 10, 31, 12, 30, 0, 0),
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'D&D session',
-      date: new Date(2021, 10, 31, 17, 30, 0, 0),
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food shopping',
-      date: new Date(2021, 11, 1, 13, 30, 0, 0),
-      reminder: false,
+  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    const getTasks = async() => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
     }
+    getTasks();
+  })
 
-  ])
+  const fetchTasks = async () => {
+    const response = await fetch('http://localhost:5000/tasks');
+    const data = await response.json();
+
+    return data;
+  }
 
   const addTask = (task) => {
     const highestId = tasks.map(task => task.id).sort()[tasks.length - 1];
