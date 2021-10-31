f36abbc5ef8c87434d571337a727a57eec19d511
Faz-PC\Faz <farronflaherty@gmail.com>
2021-10-31 15:44:07 +0000

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks';
 import Footer from './components/Footer';
+import About from './components/About';
 import AddTask from './components/AddTask';
 import { useState, useEffect } from "react"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([])
   useEffect(() => {
-    const getTasks = async() => {
+    const getTasks = async () => {
       const tasksFromServer = await fetchTasks();
       setTasks(tasksFromServer);
     }
@@ -23,7 +25,7 @@ function App() {
     return data;
   }
 
-    const fetchTask = async (id) => {
+  const fetchTask = async (id) => {
     const response = await fetch(`http://localhost:5000/tasks/${id}`);
     const data = await response.json();
 
@@ -32,31 +34,31 @@ function App() {
 
   const addTask = async (task) => {
     const response = await fetch(`http://localhost:5000/tasks`, {
-      method: 'POST', 
-      headers: { 'Content-type': 'application/json'}, 
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
       body: JSON.stringify(task)
     });
- 
+
     const data = await response.json();
-    
+
     setTasks([...tasks, data]);
   }
 
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE' })
     setTasks(tasks.filter(task => task.id !== id));
   }
 
   const toggleReminder = async (id) => {
     const taskToChange = tasks.find(task =>
       task.id === id);
-    
-    const updatedTask = {...taskToChange, reminder: !taskToChange.reminder}
+
+    const updatedTask = { ...taskToChange, reminder: !taskToChange.reminder }
 
     console.log(taskToChange);
     const response = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'PUT', 
-      headers: { 'Content-type': 'application/json'}, 
+      method: 'PUT',
+      headers: { 'Content-type': 'application/json' },
       body: JSON.stringify(updatedTask)
     });
 
@@ -72,19 +74,25 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <Header title='Faz' showAddTask={showAddTask} onToggleShowAddTask={toggleShowAddTask} />
-      {
-        showAddTask && <AddTask onAdd={addTask} />
-      }
-      {
-        tasks.length > 0
-          ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-          : <p>No tasks present</p>
-      }
-      <Footer />
-    </div>
-
+    <Router>
+      <div className="container">
+        <Header title='Faz' showAddTask={showAddTask} onToggleShowAddTask={toggleShowAddTask} />
+        <Route path='/' exact render={(props) => (
+          <>
+            {
+              showAddTask && <AddTask onAdd={addTask} />
+            }
+            {
+              tasks.length > 0
+                ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+                : <p>No tasks present</p>
+            }
+          </>
+        )} />
+      <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const About = () => {
+    return (
+        <div>
+            <h4>V1.0.0</h4>
+            <Link to="/">Go back</Link>
+            <p>This is a simple react project to familiarise myself with basic concepts</p> 
+        </div>
+    )
+}
+
+export default About
